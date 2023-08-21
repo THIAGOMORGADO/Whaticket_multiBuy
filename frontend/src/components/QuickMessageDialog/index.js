59f88bba43f3ecb2b533a1887c7e8bee19a2db17
thiago morgado <thiago_morgado@hotmail.com>
2023-08-20 22:23:56 -0300

    Atualizando Repositori Kanban

--- frontend/src/components/QuickMessageDialog/index.js
+++ frontend/src/components/QuickMessageDialog/index.js
@@ -1,172 +1,313 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Button, TextField, DialogContent, DialogActions, Grid } from '@material-ui/core';
-import PropType from 'prop-types'
-import Dialog from '../Dialog';
+import React, { useState, useEffect, useContext, useRef } from "react";
+import {
+  Button,
+  TextField,
+  DialogContent,
+  DialogActions,
+  Grid
+} from "@material-ui/core";
+import PropType from "prop-types";
+import Dialog from "../Dialog";
 import * as Yup from "yup";
 import { Formik, Form, Field } from "formik";
-import { i18n } from '../../translate/i18n';
-import { makeStyles } from '@material-ui/core/styles';
-import ButtonWithSpinner from '../ButtonWithSpinner';
+import { i18n } from "../../translate/i18n";
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonWithSpinner from "../ButtonWithSpinner";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import MicIcon from "@material-ui/icons/Mic";
 import { AuthContext } from "../../context/Auth/AuthContext";
+import ConfirmationModal from "../ConfirmationModal";
+import api from "../../services/api";
 
-import { isNil, isObject, has, get } from 'lodash';
+import { head, isNil, isObject, has, get } from "lodash";
+
+import { IconButton } from "@material-ui/core";
 
 const MessageSchema = Yup.object().shape({
-	shortcode: Yup.string()
-		.min(3, "Too Short!")
-		.max(50, "Too Long!")
-		.required("Required"),
-    message: Yup.string()
-        .min(3, "Too Short!")
-        .max(1000, "Too Long!")
-        .required("Required")
+  shortcode: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  message: Yup.string()
+    .min(3, "Too Short!")
+    .max(1000, "Too Long!")
+    .required("Required")
 });
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '350px',
-        },
-    },
-    list: {
-        width: '100%',
-        maxWidth: '350px',
-        maxHeight: '200px',
-        backgroundColor: theme.palette.background.paper,
-    },
-    inline: {
-        width: '100%'
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "350px"
     }
+  },
+  list: {
+    width: "100%",
+    maxWidth: "350px",
+    maxHeight: "200px",
+    backgroundColor: theme.palette.background.paper
+  },
+  inline: {
+    width: "100%"
+  }
 }));
 
-
 function QuickMessageDialog(props) {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    const initialMessage = {
-        id: null,
-        shortcode: '',
-        message: ''
-    };
+  const initialMessage = {
+    id: null,
+    shortcode: "",
+    message: "",
+    sendAsVoiceNote: false
+  };
 
-    const { modalOpen, saveMessage, editMessage, onClose, messageSelected } = props;
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [message, setMessage] = useState(initialMessage);
-    const [loading, setLoading] = useState(false);
+  const {
+    modalOpen,
+    saveMessage,
+    editMessage,
+    onClose,
+    messageSelected,
+    mediaDelete
+  } = props;
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [message, setMessage] = useState(initialMessage);
+  const [loading, setLoading] = useState(false);
+  const [attachment, setAttachment] = useState(null);
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const attachmentFile = useRef(null);
 
-    const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
-    useEffect(() => {
-        verifyAndSetMessage()
-        setDialogOpen(modalOpen)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [modalOpen])
+  useEffect(() => {
+    verifyAndSetMessage();
+    setDialogOpen(modalOpen);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [modalOpen]);
 
-    useEffect(() => {
-        verifyAndSetMessage()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [messageSelected])
+  useEffect(() => {
+    verifyAndSetMessage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [messageSelected]);
 
-    const messageSelectedIsValid = () => {
-        return isObject(messageSelected) && has(messageSelected, 'id') && !isNil(get(messageSelected, 'id'))
-    }
-
-    const verifyAndSetMessage = () =>{
-        if (messageSelectedIsValid()) {
-            const { id, message, shortcode } = messageSelected
-            setMessage({ id, message, shortcode })
-        } else {
-            setMessage(initialMessage)
-        }
-    }
-
-    const handleClose = () => {
-        onClose()
-        setLoading(false)
-    }
-
-    const handleSave = async (values) => {
-        if (messageSelectedIsValid()) {
-            editMessage({
-                ...messageSelected,
-                ...values,
-                userId: user.id
-            });
-        } else {
-            saveMessage({
-                ...values,
-                userId: user.id
-            });
-        }
-        handleClose()
-    }
-
+  const messageSelectedIsValid = () => {
     return (
-        <Dialog
-            title="Mensagem Rápida"
-            modalOpen={dialogOpen}
-            onClose={handleClose}
-        >
-            <Formik
-                initialValues={message}
-                enableReinitialize={true}
-                validationSchema={MessageSchema}
-                onSubmit={(values, actions) => {
-                    setLoading(true)
-                    setTimeout(() => {
-                        handleSave(values);
-                        actions.setSubmitting(false);
-                    }, 400);
-                }}
-            >
+      isObject(messageSelected) &&
+      has(messageSelected, "id") &&
+      !isNil(get(messageSelected, "id"))
+    );
+  };
 
-                {({ touched, errors }) => (
-                    <Form>
-                        <DialogContent className={classes.root} dividers>
-                            <Grid direction="column" container>
-                                <Grid item>
-                                    <Field
-                                        as={TextField}
-                                        name="shortcode"
-                                        label={i18n.t("quickMessages.dialog.shortcode")}
-                                        error={touched.shortcode && Boolean(errors.shortcode)}
-                                        helperText={touched.shortcode && errors.shortcode}
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                                <Grid item>
-                                    <Field
-                                        as={TextField}
-                                        name="message"
-                                        rows={6}
-                                        label={i18n.t("quickMessages.dialog.message")}
-                                        multiline={true}
-                                        error={touched.message && Boolean(errors.message)}
-                                        helperText={touched.message && errors.message}
-                                        variant="outlined"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleClose} color="primary">
-                                Cancelar
-                            </Button>
-                            <ButtonWithSpinner loading={loading} color="primary" type="submit" variant="contained" autoFocus>
-                                Salvar
-                            </ButtonWithSpinner>
-                        </DialogActions>
-                    </Form>
+  const verifyAndSetMessage = () => {
+    if (messageSelectedIsValid()) {
+      const { id, message, shortcode, mediaPath, mediaName, sendAsVoiceNote } =
+        messageSelected;
+      setMessage({
+        id,
+        message,
+        shortcode,
+        mediaPath,
+        mediaName,
+        sendAsVoiceNote
+      });
+      if (mediaPath) {
+        setAttachment({
+          name: mediaName,
+          isAudioFile: isAudio({ name: mediaPath })
+        });
+      }
+    } else {
+      setMessage(initialMessage);
+    }
+  };
+
+  const handleClose = () => {
+    setAttachment(null);
+    onClose();
+    setLoading(false);
+  };
+
+  const isAudio = file => {
+    if (file.name.endsWith(".mp3")) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleAttachmentFile = e => {
+    const file = head(e.target.files);
+    if (file) {
+      file.isAudioFile = isAudio(file);
+      setAttachment(file);
+    }
+  };
+
+  const handleAttachmentRemove = async () => {
+    if (attachment) {
+      setAttachment(null);
+      attachmentFile.current.value = null;
+    }
+
+    if (message.mediaName) {
+      await mediaDelete(message.id);
+      setAttachment(null);
+      setMessage(v => ({ ...v, mediaPath: null, mediaName: null }));
+    }
+  };
+
+  const handleSave = async values => {
+    if (messageSelectedIsValid()) {
+      editMessage(
+        {
+          ...messageSelected,
+          ...values,
+          userId: user.id
+        },
+        attachment
+      );
+    } else {
+      saveMessage(
+        {
+          ...values,
+          userId: user.id
+        },
+        attachment
+      );
+    }
+    handleClose();
+  };
+
+  return (
+    <>
+      <ConfirmationModal
+        title={i18n.t("scheduleModal.title.delete")}
+        open={deleteConfirmationOpen}
+        onClose={() => setDeleteConfirmationOpen(false)}
+        onConfirm={() => handleAttachmentRemove()}
+      >
+        {i18n.t("scheduleModal.deleteMessage")}
+      </ConfirmationModal>
+      <Dialog
+        title="Mensagem Rápida"
+        modalOpen={dialogOpen}
+        onClose={handleClose}
+      >
+        <Formik
+          initialValues={message}
+          enableReinitialize={true}
+          validationSchema={MessageSchema}
+          onSubmit={(values, actions) => {
+            setLoading(true);
+            setTimeout(() => {
+              handleSave(values);
+              actions.setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ touched, errors }) => (
+            <Form>
+              <DialogContent className={classes.root} dividers>
+                <Grid direction="column" container>
+                  <Grid item>
+                    <Field
+                      as={TextField}
+                      name="shortcode"
+                      label={i18n.t("quickMessages.dialog.shortcode")}
+                      error={touched.shortcode && Boolean(errors.shortcode)}
+                      helperText={touched.shortcode && errors.shortcode}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <Grid item>
+                    <Field
+                      as={TextField}
+                      name="message"
+                      rows={6}
+                      label={i18n.t("quickMessages.dialog.message")}
+                      multiline={true}
+                      error={touched.message && Boolean(errors.message)}
+                      helperText={touched.message && errors.message}
+                      variant="outlined"
+                    />
+                  </Grid>
+                  <div style={{ display: "none" }}>
+                    <input
+                      type="file"
+                      ref={attachmentFile}
+                      accept=".mp3,.png,.jpg,.mp4,.pdf"
+                      onChange={e => handleAttachmentFile(e)}
+                    ></input>
+                  </div>
+                  {attachment && (
+                    <div>
+                      <Button
+                        onClick={() => {
+                          if (!attachment.isAudioFile) return;
+                          setMessage(v => ({
+                            ...v,
+                            sendAsVoiceNote: !v.sendAsVoiceNote
+                          }));
+                        }}
+                        startIcon={
+                          message.sendAsVoiceNote ? (
+                            <MicIcon />
+                          ) : (
+                            <AttachFileIcon />
+                          )
+                        }
+                      >
+                        {attachment?.name}
+                      </Button>
+                      <IconButton
+                        onClick={() => setDeleteConfirmationOpen(true)}
+                        color="secondary"
+                      >
+                        <DeleteOutlineIcon />
+                      </IconButton>
+                    </div>
+                  )}
+                </Grid>
+              </DialogContent>
+              <DialogActions>
+                {!attachment && !message.mediaPath && (
+                  <Button
+                    color="primary"
+                    onClick={() => attachmentFile.current.click()}
+                    variant="outlined"
+                  >
+                    {i18n.t("announcements.dialog.buttons.attach")}
+                  </Button>
                 )}
-            </Formik>
-        </Dialog>
-    )
+                <Button
+                  onClick={handleClose}
+                  color="secondary"
+                  variant="outlined"
+                >
+                  Cancelar
+                </Button>
+                <ButtonWithSpinner
+                  loading={loading}
+                  color="primary"
+                  type="submit"
+                  variant="contained"
+                  autoFocus
+                >
+                  Salvar
+                </ButtonWithSpinner>
+              </DialogActions>
+            </Form>
+          )}
+        </Formik>
+      </Dialog>
+    </>
+  );
 }
 
 QuickMessageDialog.propType = {
-    modalOpen: PropType.bool,
-    onClose: PropType.func
-}
+  modalOpen: PropType.bool,
+  onClose: PropType.func
+};
 
 export default QuickMessageDialog;
--- frontend/src/pages/Multianban/index.js
+++ frontend/src/pages/Multianban/index.js
@@ -14,7 +14,7 @@ import "./responsive.css";
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
-    flexDirection: "column", // Alterado para coluna em telas menores
+    flexDirection: "row", // Alterado para coluna em telas menores
     alignItems: "center", // Centralizar elementos horizontalmente
     backgroundColor: "#3179ba",
     padding: theme.spacing(1)
@@ -66,13 +66,13 @@ const MultiKanban = () => {
     draggable: true
   };
   const [jsonModalValues, setJsonModalValues] = useState({});
-  const [jsonModalTagsInfo, setJsonModalTagsInfo] = useState({});
+  const [jsonModalTagsInfo, setJsonModalTagsInfo] = useState([]);
   const [jsonModalExtraInfo, setJsonModalExtraInfo] = useState({});
   const [fetchData, setFetchData] = useState([]);
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [extraInfoLoading, setExtraInfoLoading] = useState(true);
   const [fetchExtraInfos, setFetchExtraInfos] = useState([]);
-
+  const [profilePicUrlImg, setprofilePicUrlImg] = useState([]);
   const [laneList, setLaneList] = useState({
     laneListStorage: []
   });
@@ -83,10 +83,12 @@ const MultiKanban = () => {
         id: "lane1",
         title: "Novos Leads",
         label: "(0)",
+
         cards: []
       }
     ]
   });
+
   const fetchTickets = async () => {
     try {
       const { data } = await api.get("/tickets", {});
@@ -95,24 +97,37 @@ const MultiKanban = () => {
       return [];
     }
   };
+  const saveBoardDataToLocalStorage = newData => {
+    localStorage.setItem("laneDate", JSON.stringify(newData));
+  };
 
   const popularCards = async () => {
+    const response = await api.get(`/tickets`);
+    const img = fetchData.map(data => {
+      console.log(data);
+    });
+    console.log("Aqui e response ", JSON.stringify(img));
     try {
       const tickets = await fetchTickets();
+      console.log(tickets);
+
       setFetchData(tickets);
+
       const cards = tickets
         .filter(ticket => ticket.status === "open")
         .map(ticket => ({
           id: ticket.id.toString(),
-          title: "Ticket nº " + ticket.id.toString(),
+          title: ticket.contact.name,
           description: ticket.contact.number,
           draggable: true,
+          avatar: "passa a imagem aqui",
           tags: ticket.tags?.map(tag => ({
             id: tag.id.toString(),
             bgcolor: tag.color,
             title: tag.name
           }))
         }));
+
       setFile(prevFile => ({
         ...prevFile,
         lanes: prevFile.lanes.map(lane => {
@@ -122,9 +137,11 @@ const MultiKanban = () => {
               cards: cards
             };
           }
+          console.log(tickets);
           return lane;
         })
       }));
+      console.log(cards);
     } catch (err) {
       console.log(err);
     }
@@ -132,99 +149,52 @@ const MultiKanban = () => {
 
   useEffect(() => {
     popularCards();
+    getCardList();
+    const savedData = localStorage.getItem("laneDate");
+    if (savedData) {
+      setFile(JSON.parse(savedData));
+    }
+  }, []);
 
-    loadingStorage();
-  }, [popularCards]);
+  const handleCardMove = async (cardId, sourceLaneId, targetLaneId) => {
+    const updatedData = { ...file };
+    const sourceLane = updatedData.lanes.find(lane => lane.id === sourceLaneId);
+    const targetLane = updatedData.lanes.find(lane => lane.id === targetLaneId);
+    const movedCard = sourceLane.cards.find(card => card.id === cardId);
 
-  const handleCardMove = (cardId, sourceLaneId, targetLaneId) => {
-    console.log("cardId:", cardId);
-    console.log("sourceLaneId:", sourceLaneId);
-    console.log("targetLaneId:", targetLaneId);
-
-    setFile(prevFile => {
-      const updatedLanes = prevFile.lanes.map(lane => {
-        if (lane.id === sourceLaneId) {
-          console.log("Lane.id === sourceLaneId");
-          return {
-            ...lane,
-            cards: lane.cards.filter(card => card.id !== cardId)
-          };
-        }
-        if (lane.id === targetLaneId) {
-          console.log("lane.id === targetLaneId");
-          return {
-            ...lane,
-            cards: [
-              ...lane.cards,
-              prevFile.lanes
-                .find(l => l.id === sourceLaneId)
-                .cards.find(c => c.id === cardId)
-            ]
-          };
-        }
-        return lane;
-      });
-
-      return {
-        ...prevFile,
-        lanes: updatedLanes
-      };
-    });
+    if (movedCard) {
+      sourceLane.cards = sourceLane.cards.filter(card => card.id !== cardId);
+      targetLane.cards.splice(position, 0, { ...movedCard });
+      setFile(updatedData);
+      localStorage.setItem("laneDate", JSON.stringify(laneFiltered));
+    }
   };
 
+  function getCardList() {
+    JSON.parse(localStorage.getItem("moveCard"));
+  }
   function handleNewLaneAdd(title) {
     const laneNew = title;
-
-    const laneDate = {
-      id: laneNew.id,
-      title: laneNew.title
+    const updatedData = { ...file };
+    const newLane = {
+      id: Date.now().toString(),
+      title: laneNew.title,
+      cards: []
     };
-
-    setFile({
-      lanes: [...file.lanes, laneDate]
-    });
-
-    file.lanes.push(laneDate);
-    if (laneList.laneListStorage.length > 0) {
-      laneList.laneListStorage = [];
-    }
-    file.lanes.map(value => {
-      if (value.id != "lane1") {
-        laneList.laneListStorage.push(value);
-      }
-    });
-    localStorage.setItem("laneDate", JSON.stringify(laneList.laneListStorage));
-
-    console.log(file);
-  }
-
-  async function loadingStorage() {
-    const response = await JSON.parse(localStorage.getItem("laneDate"));
-    if (response) {
-      response.map(value => {
-        if (value && value["id"] && value["title"]) {
-          const newLane = {
-            id: value["id"],
-            title: value["title"],
-            cards: []
-          };
-          setFile(prevFile => ({
-            ...prevFile,
-            lanes: [...prevFile.lanes, newLane]
-          }));
-        }
-      });
-    }
+    updatedData.lanes.push(newLane);
+    setFile(updatedData);
   }
 
   async function openModal(cardId) {
     setIsOpen(true);
     console.log(cardId);
     const data = fetchData.find(value => value["id"] == cardId);
+    const tags = fetchData.find(item => item.id === cardId);
+    setJsonModalTagsInfo(tags); // Que esta retornando aqui
     setJsonModalValues(data["contact"]);
-    setJsonModalTagsInfo(data["tags"]);
     setJsonModalExtraInfo(data["contact"]);
     setJsonModalTagsInfo(data["tags"]);
+
     const response = await api.get(`/contacts/${cardId}`);
     setFetchExtraInfos(response.data.extraInfo);
   }
@@ -248,6 +218,7 @@ const MultiKanban = () => {
         onCardClick={openModal}
         onCardMoveAcrossLanes={handleCardMove}
         onLaneDelete={handleRemoveLane}
+        onDataChange={saveBoardDataToLocalStorage}
         className={classes.tab}
       />
       <div className={customStyles.mobile}>
@@ -257,7 +228,7 @@ const MultiKanban = () => {
           onRequestClose={closeModal}
         >
           <div style={customStyles.modalHeader}>
-            <h2>Detalhes do cardId {jsonModalExtraInfo["name"]}</h2>
+            <h2>Detalhes. {jsonModalExtraInfo["name"]}</h2>
             <button
               onClick={closeModal}
               style={{ border: "none", background: "none" }}
@@ -280,7 +251,13 @@ const MultiKanban = () => {
             <div style={customStyles.warpper}>
               <h3>Nome : {jsonModalValues["name"]}</h3>
               <p>email: {jsonModalValues["email"]}</p>
-              <div>Tags: {jsonModalTagsInfo["name"]}</div>
+              {jsonModalTagsInfo.map(item => (
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <p style={{ background: `${item.color}`, color: "#fff" }}>
+                    Tags: {item.name}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
           <div style={customStyles.otherInfos}>
@@ -290,7 +267,7 @@ const MultiKanban = () => {
               fetchExtraInfos.map(v => (
                 <div key={v.id}>
                   <div style={customStyles.listArea}>
-                    <span>{v.name}</span> <span> {v.value}</span>
+                    <span>{v.name}</span> : <span> {v.value}</span>
                   </div>
                 </div>
               ))
@@ -309,114 +286,42 @@ const MultiKanban = () => {
   );
 };
 
-export const customStylesMobile = {
-  mobile: {
-    width: "100%",
-    maxWidth: "400px" // Corrigido o nome da propriedade (maxWidth em vez de maxWidht)
-  },
-
+const customStyles = {
   content: {
-    width: "50%",
-    minWidth: "50px",
-    maxWidth: "600px", // Ajuste a largura máxima da modal conforme necessário
-    height: "auto", // Definido como "auto" para se ajustar ao conteúdo
-    borderRadius: "5px",
+    with: "80vw",
     top: "50%",
     left: "50%",
     right: "auto",
     bottom: "auto",
     marginRight: "-50%",
-    transform: "translate(-50%, -50%)"
-  },
-
-  contactArea: {
-    maxWidht: "100%",
-    marginTop: "5%",
+    transform: "translate(-50%, -50%)",
     display: "flex",
-    marginLeft: "10%",
+    flexDirection: "column",
     alignItems: "center"
   },
   modalHeader: {
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  logo: {
-    width: "10%",
-    height: "10%",
-    borderRadius: "50%"
-  },
-  warpper: {
-    marginLeft: "5%"
-  },
-  otherInfos: {
-    marginTop: "5%"
-  },
-  footer: {
-    background: "red",
-    display: "flex",
     width: "100%",
-    flexDirection: "row"
-  },
-  extraInfoLoading: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: "10%",
-    color: "#ddd"
-  }
-};
-
-export const customStyles = {
-  mobile: {
-    width: "100%",
-    maxWidht: "400px"
-  },
-  content: {
-    width: "80%",
-    minWidth: "50px",
-    height: "50%",
-    borderRadius: "5px",
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)"
+    justifyContent: "space-between",
+    flexDirection: "row"
   },
   contactArea: {
-    maxWidht: "100%",
-    marginTop: "5%",
     display: "flex",
-    marginLeft: "10%",
-    alignItems: "center"
-  },
-  modalHeader: {
-    display: "flex",
-    justifyContent: "space-between"
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: "1rem",
+    alignItems: "center",
+    marginTop: "10px",
+    marginBottom: "10px"
   },
   logo: {
-    width: "10%",
-    height: "10%",
-    borderRadius: "50%"
-  },
-  warpper: {
-    marginLeft: "5%"
+    width: "30%",
+    height: "30%"
   },
   otherInfos: {
-    marginTop: "5%"
-  },
-  footer: {
-    background: "red",
     display: "flex",
-    width: "100%",
-    flexDirection: "row"
-  },
-  extraInfoLoading: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: "10%",
-    color: "#ddd"
+    flexDirection: "column"
   }
 };
 
